cce7d8644d17bbb1ee17af7eaec26c4ad2366238
Tatana Prudnikova <talianensis@gmail.com>
2021-11-28 23:46:06 +0100

    finalizace

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("css");
+}
+module.exports = function(eleventyConfig) {
+    // ...necháme, co už v konfiguraci je, a přidáme:
+    return {
+      // možné formáty šablon
+      templateFormats: ["njk", "html", "md"],
+      // jako šablonovací jazyk zvolíme Nunjucks`
+      markdownTemplateEngine: "njk",
+      htmlTemplateEngine: "njk",
+      dataTemplateEngine: "njk",
+    }
+    
+};
+  
